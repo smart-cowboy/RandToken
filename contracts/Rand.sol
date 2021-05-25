a6893f5d8feb062fa27569c118d5ed13d91dde0e
Smart Cowboy <dreamall2021.global@gmail.com>
2021-05-26 02:15:09 +0900

    Rand Token name changed

--- contracts/Rand.sol
+++ contracts/Rand.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract DappToken is ERC20 {
+contract Rand is ERC20 {
     constructor() public ERC20("RAND Token", "RAND") {
     }
 
